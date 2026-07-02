d7e59f76132fa316ac0267c1a71f2bbc39d3ae3d
Olabi Elore <olabielore@gmail.com>
2026-07-02 16:35:14 +0300

    fix: deleted the files from commit error

--- app/(main)/stories/new/page.tsx
+++ app/(main)/stories/new/page.tsx
@@ -1,28 +1,5 @@
-
-import { Metadata } from 'next';
 import AddStoryForm from '@/components/AddStoryForm/AddStoryForm';
-
-export const metadata: Metadata = {
-  title: 'Створити нову історію',
-};
 
 export default function NewStoryPage() {
   return <AddStoryForm />;
 }
-
-import css from "./SaveStory.module.css"
-import Image from "next/image";
-
-export default function CreateNewStory() {
-
-  return (
-    <main className={css.main}>
-        <div className={css.container}>
-            <p>Обкладинка статті</p>
-            <h1 className={css.title}>Створити нову історію</h1>
-            <Image alt="Обкладинка статті" src={preview ?? '/placeholder.jpg'} width={1091} height={726} />
-            <button>Завантажити фото</button>
-      </div>
-    </main>
-  )
-};
